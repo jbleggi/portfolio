a1d1d141998e342819251e51220564eb8fc57af3
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-04-29 21:58:51 -0400

    Configure Vite and GH Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import ProjectsView from '../views/ProjectsView.vue'
-import ResumeView from '../views/ResumeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import ProjectsView from '@/views/ProjectsView.vue'
+import ResumeView from '@/views/ResumeView.vue'
 import WorkGalleryView from '@/views/WorkGalleryView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
-  { path: '/about', name: 'about', component: AboutView },
   { path: '/projects', name: 'projects', component: ProjectsView },
   { path: '/resume', name: 'resume', component: ResumeView },
   { path: '/on-the-job', name: 'on-the-job', component: WorkGalleryView }
 ]
 
 export default createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  history: createWebHashHistory(),
+  routes,
 })
